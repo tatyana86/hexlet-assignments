2f70581af16dff248d27466c07407fefb42970f9
@hexlet/cli <support@hexlet.io>
2023-12-12 23:13:10 +0400

    submit java-spring-ru/jpa-comparing-entities

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
@@ -1,5 +1,5 @@
 package exercise.controller;
-
+import org.springframework.data.domain.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
