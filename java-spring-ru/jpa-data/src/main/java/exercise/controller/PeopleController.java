ad07a2173deb26812fff50706f878a572bee9ecc
@hexlet/cli <support@hexlet.io>
2023-12-12 21:58:53 +0400

    submit java-spring-ru/jpa-data

--- java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -2,18 +2,15 @@ package exercise.controller;
 
 import exercise.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+//import java.lang.foreign.Linker;
 import java.util.List;
 import java.util.Optional;
+
 import exercise.model.Person;
 
 @RestController
@@ -29,7 +26,6 @@ public class PeopleController {
     }
 
     // BEGIN
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Person> index(@RequestParam(defaultValue = "10") Integer limit) {
@@ -38,8 +34,8 @@ public class PeopleController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Person create(@Validated @RequestBody Person person) {
-        var res = personRepository.save(person);
+    public Person create (@Validated @RequestBody Person p) {
+        var res = personRepository.save(p);
         return res;
     }
 
@@ -48,6 +44,5 @@ public class PeopleController {
         var user = personRepository.findById(id).get();
         personRepository.delete(user);
     }
-
     // END
 }
