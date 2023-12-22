d6e3d603f694ee82302815db6021f2defb925d4a
@hexlet/cli <support@hexlet.io>
2023-12-22 19:43:03 +0400

    submit java-spring-ru/jpa-repository

--- java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
+++ java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import exercise.model.Product;
+import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 
