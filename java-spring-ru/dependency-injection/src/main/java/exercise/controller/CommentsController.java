9924582f304d9158ffad8f1d942baea74b0d4dc8
@hexlet/cli <support@hexlet.io>
2023-12-22 20:23:27 +0400

    submit java-spring-ru/dependency-injection

--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -1,15 +1,7 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import exercise.model.Post;
 import java.util.List;
@@ -21,7 +13,7 @@ import exercise.exception.ResourceNotFoundException;
 // BEGIN
 @RestController
 @RequestMapping("/comments")
-public class PostsController {
+public class CommentsController {
     @Autowired
     private CommentRepository commentRepository;
 
