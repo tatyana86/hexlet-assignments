6549d1cf5c18799e16b2383efa51fe1acd7527b1
@hexlet/cli <support@hexlet.io>
2023-12-30 11:24:37 +0400

    submit java-spring-ru/entity-validation

---
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -0,0 +1,36 @@
+package exercise.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+// BEGIN
+Setter
+@Getter
+public class GuestCreateDTO {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "\\+[0-9]{11,13}")
+    private String phoneNumber;
+
+    @Pattern(regexp = "\\d{4}")
+    private String clubCard;
+
+    @Future
+    private LocalDate cardValidUntil;
+// END
