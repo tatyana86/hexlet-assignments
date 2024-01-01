89020e67fa51fcfb90eb893a0de6419772c2a1df
@hexlet/cli <support@hexlet.io>
2024-01-01 19:03:02 +0400

    submit java-spring-ru/one-to-many

---
+++ java-spring-ru/one-to-many/src/main/java/exercise/model/Task.java
@@ -0,0 +1,42 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "assignees")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    // BEGIN
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User assignee;
+    // END
+}
