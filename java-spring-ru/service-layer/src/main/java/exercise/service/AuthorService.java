65d5f394af2a336fb3c387c0d32967783792bfb1
@hexlet/cli <support@hexlet.io>
2024-01-02 18:14:03 +0400

    submit java-spring-ru/service-layer

--- java-spring-ru/service-layer/src/main/java/exercise/service/AuthorService.java
+++ java-spring-ru/service-layer/src/main/java/exercise/service/AuthorService.java
@@ -8,6 +8,7 @@ import exercise.mapper.AuthorMapper;
 import exercise.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
